1a7bd1eebea5b8d406e1618241ea9b8c552718b6
Enzo Soares <enzocaue93@gmail.com>
2022-02-28 11:26:37 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,8 @@ function checkInputs() {
     const firstNameValue = firstName.value;
     const lastNameValue = lastName.value;
     const emailValue = email.value;
-    // const password = password.value
+    const passwordValue = password.value;
+    // const passwordConfirmationValue = passwordConfirmation.value;
 
     if (firstNameValue === "") {
         setErrorFor(firstName, "First name is required.");
@@ -29,19 +30,35 @@ function checkInputs() {
 
     if (emailValue === "") {
         setErrorFor(email, "Email is required.");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Please, enter a valid email.");
     } else {
         setSuccessFor(email);
+    }
+
+    if (passwordValue === "") {
+        setErrorFor(password, "Incorrect password.");
+    } else {
+        setSuccessFor(password);
     }
 }
 
 function setErrorFor(input, message) {
     const formInput = input.parentElement;
 
+    // add class name error
     formInput.className = "form_input error";
 }
 
 function setSuccessFor(input) {
     const formInput = input.parentElement;
 
+    // add class name success
     formInput.className = "form_input success"
+}
+    //email validation
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
 }
